86145fece1f4f0def1dc7ee5a2746c9ac1453cca
Oana-Poran <oanaporan@gmail.com>
2018-08-22 12:36:14 +0300

    Spec added to RSS Feeds Suite.

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -1,60 +1,75 @@
 /* feedreader.js
  *
  * This is the spec file that Jasmine will read and contains
- * all of the tests that will be run against your application.
+ * all of the tests to be run against the application.
  */
 
-/* We're placing all of our tests within the $() function,
- * since some of these tests may require DOM elements. We want
- * to ensure they don't run until the DOM is ready.
+/* Placing all of the tests within the $() function,
+ * since some of these tests may require DOM elements, to ensure they don't run until the DOM is ready.
  */
 $(function() {
-    /* This is our first test suite - a test suite just contains
-    * a related set of tests. This suite is all about the RSS
-    * feeds definitions, the allFeeds variable in our application.
-    */
+    // This suite is all about the RSS feeds definitions,
+    // the allFeeds variable in the application.
     describe('RSS Feeds', function() {
-        /* This is our first test - it tests to make sure that the
-         * allFeeds variable has been defined and that it is not
-         * empty. Experiment with this before you get started on
-         * the rest of this project. What happens when you change
-         * allFeeds in app.js to be an empty array and refresh the
-         * page?
-         */
+        // Tests to make sure that the allFeeds variable has been 
+        // defined and that it is not empty.
         it('are defined', function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
+        // Loops through each feed
+        //  in the allFeeds object and ensures it has a URL defined
+        //   and that the URL is not empty.
+        allFeeds.forEach(function(feed) {
+            it('each feed has a URL defined', function() {
+                expect(feed).toContain(URL);
+            })
+        })
+       
 
 
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
-
-
-        /* TODO: Write a test that loops through each feed
+        /* Loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('each feed has a name defined', function() {
+            expect()
+        })
     });
 
 
-    /* TODO: Write a new test suite named "The menu" */
+    /* The menu suite */
+    describe('The menu', function(){
+        // ensures the menu element is
+        // hidden by default.
+        it('nemu hidden', function(){
 
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+        })
+        // Ensures the menu changes visibility when the menu icon is clicked. 
+        // Has two expectations: 
+        // - does the menu display when clicked 
+        // - does it hide when clicked again.
+        it('menu hidden', function(){
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
+        })
+        
+        it('menu icon appears', function(){
 
-    /* TODO: Write a new test suite named "Initial Entries" */
+        })
+
+    })
+
+    //Initial Entries Suite //
+    describe('Initial Entries', function(){
+        // Ensures when the loadFeed
+        // function is called and completes its work, there is at least
+        // a single .entry element within the .feed container.
+        // it('loadFeed completed and has at least one entry', function(){
+
+        })
+
+
+    })
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
